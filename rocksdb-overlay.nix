7fea7c48ef19b8e8ae9eedcf4394ac8fe468d43f
Riccardo Casatta <riccardo@casatta.it>
2026-01-26 11:51:07 +0100

    nix: use matching version of rocksdb

--- rocksdb-overlay.nix
+++ rocksdb-overlay.nix
@@ -1,13 +1,13 @@
 final: prev: {
 
   rocksdb = prev.rocksdb.overrideAttrs (oldAttrs: rec {
-    version = "8.1.1";
+    version = "10.4.2";
 
     src = final.fetchFromGitHub {
       owner = "facebook";
       repo = oldAttrs.pname;
       rev = "v${version}";
-      hash = "sha256-79hRtc5QSWLLyjRGCmuYZSoIc9IcIsnl8UCinz2sVw4=";
+      hash = "sha256-mKh6zsmxsiUix4LX+npiytmKvLbo6WNA9y4Ns/EY+bE=";
     };
   });
 }
