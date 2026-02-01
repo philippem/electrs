34de949dd0f1b86ec2cc2ae8e23db1a1cbe0ee32
Nadav Ivgi <nadav@shesek.info>
2026-02-02 00:09:32 +0200

    Update tests to use Electrum v4.6.2
    - Update `electrumd` to support newer Electrum releases
    
      The changes in `electrumd` can be reviewed here: https://github.com/shesek/electrumd/compare/b35d9db285d932cb3c2296beab65e571a2506349..9897271b3538ef1e20166ec870746af8167a8d4b
    
    - Update to new JSON response format for `onchain_history`
    
    - Electrum v4.6 appears to process new blocks/transactions significantly
      slower compared to v4.5. This initially required increasing the
      sleep() in `notify_wallet` up to several seconds for the tests to
      pass, however I found that there's a new `wait_for_rpc` command
      that can be used instead: https://github.com/spesmilo/electrum/commit/f3551f3c25b0f570559e41046b34e38a65cf6823

--- tests/electrum.rs
+++ tests/electrum.rs
@@ -52,6 +52,7 @@ impl WalletTester {
     fn notify_wallet(&self) {
         self.electrum_server.notify();
         std::thread::sleep(std::time::Duration::from_millis(200));
+        self.electrum_wallet.call("wait_for_sync", &json!([])).unwrap();
     }
 
     fn assert_balance(&self, confirmed: f64, unconfirmed: f64) {
@@ -142,18 +143,18 @@ fn test_electrum_history() -> Result<()> {
     let history = wt.electrum_wallet.call("onchain_history", &json!([]))?;
     log::debug!("history = {:#?}", history);
     assert_eq!(
-        history["transactions"][0]["txid"].as_str(),
+        history[0]["txid"].as_str(),
         Some(txid1.to_string().as_str())
     );
-    assert_eq!(history["transactions"][0]["height"].as_u64(), Some(102));
-    assert_eq!(history["transactions"][0]["bc_value"].as_str(), Some("0.1"));
+    assert_eq!(history[0]["height"].as_u64(), Some(102));
+    assert_eq!(history[0]["bc_value"].as_str(), Some("0.1"));
 
     assert_eq!(
-        history["transactions"][1]["txid"].as_str(),
+        history[1]["txid"].as_str(),
         Some(txid2.to_string().as_str())
     );
-    assert_eq!(history["transactions"][1]["height"].as_u64(), Some(103));
-    assert_eq!(history["transactions"][1]["bc_value"].as_str(), Some("0.2"));
+    assert_eq!(history[1]["height"].as_u64(), Some(103));
+    assert_eq!(history[1]["bc_value"].as_str(), Some("0.2"));
 
     Ok(())
 }
